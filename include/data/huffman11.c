c07efa5172c0a05334e541d953987899a8b9ddc5
peterbjornx <me@pbx.sh>
2020-04-29 14:17:45 +0200

    Implement huffman decoder in FastSPI

---
+++ include/data/huffman11.c
@@ -0,0 +1,8 @@
+//
+// Created by pbx on 29/04/20.
+//
+#include "fastspi.h"
+
+huff_map_t huff11_map[0x8000][2] = {
+#include "data/hufftbl11.c"
+};
--- include/fastspi.h
+++ include/fastspi.h
@@ -20,6 +20,15 @@ typedef struct {
     uint32_t  par_data[0x200];
 } spi_component_t;
 
+#define SPI_HDLUT_CACHE_SIZE (0x1000)
+#define SPI_HDBLK_CACHE_SIZE (0x20)
+
+typedef struct {
+    uint32_t sz;
+    uint32_t cs;
+    uint8_t  arr[32];
+} huff_map_t;
+
 typedef struct {
     device_instance self;
     pci_simplefunc func;
@@ -34,12 +43,23 @@ typedef struct {
     int          spi_image_file;
     uint32_t     spi_opmenu[2];
     uint32_t     sai;
-    uint8_t spi_read_buffer[0x40];
+    uint8_t      spi_read_buffer[0x40];
+    uint32_t     spi_hdcompoff;
+    uint32_t     cached_hdlut_base;
+    uint32_t     cached_hdlut_size;
+    uint32_t     cached_hdlut[SPI_HDLUT_CACHE_SIZE / sizeof(uint32_t)];
+    uint32_t     cached_hdblk_base;
+    uint8_t      cached_hdblk[SPI_HDBLK_CACHE_SIZE];
+    uint8_t      page_buf[ 4096 ];
+
+
 } fastspi_inst;
 
 void spi_openimg( fastspi_inst *spi, const char *path );
 int spi_write( fastspi_inst *spi, int addr, const void *buffer, int count );
 int spi_read( fastspi_inst *spi, int addr, void *buffer, int count );
 int spi_direct_read( fastspi_inst *spi, int addr, void *buffer, int count );
-
+int spi_huffmann_read( fastspi_inst *spi, int addr, void *buffer, int count );
+int spi_readimg( fastspi_inst *spi, int start, int count, void *buffer );
+int spi_read_csme( fastspi_inst *spi, int start, int count, void *buffer );
 #endif //MELOADER_FASTSPI_H
